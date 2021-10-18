444aba6c1d1f1bf62b5f62a6c959206b9c9fc00d
TrashMonsterAbe <aianavisca@massart.edu>
2021-10-17 21:05:19 -0400

    Scene4_updates

--- FA21_StoryB/Assets/Scripts/Dialog_Scripts/Dialogue4a.cs
+++ FA21_StoryB/Assets/Scripts/Dialog_Scripts/Dialogue4a.cs
@@ -15,28 +15,53 @@ public class Dialogue4a : MonoBehaviour
       //public Text Char3name;
       //public Text Char3speech;
       public GameObject dialogue;
-      //public GameObject ArtChar1;
-      public GameObject ArtBG1;
-      //public GameObject Choice1a;
-      //public GameObject Choice1b;
+	  public GameObject ImageName;
+	  public GameObject ArtAssets;
+	  public GameObject RedEyes;
+	  public GameObject Growl;
+	  public GameObject Normal;
+	  public GameObject OpenMouth;
+	  public GameObject NormalDark;
+	  public GameObject ArtBGUnderPorch;
+	  public GameObject ArtBGCoverBroken;
+	  public GameObject ArtBGCover;
+	  public GameObject ArtBGCover75;
+      public GameObject Choice4b;
+      public GameObject Choice4c;
       public GameObject NextScene1Button;
       public GameObject NextScene2Button;
-	  //public GameObject NextScene3Button;
+	  public GameObject Choice4d;
+	  public GameObject Choice4e;
       public GameObject nextButton;
+	  public GameObject TextBox1;
+	  public GameObject TextBox2;
+	  public GameObject BackFlat;
       public GameHandler gameHandler;
      //public AudioSource audioSource;
       private bool allowSpace = true;
 
 void Start(){         // initial visibility settings
       dialogue.SetActive(false);
-      //ArtChar1.SetActive(false);
-      ArtBG1.SetActive(true);
-      //Choice1a.SetActive(false);
-      //Choice1b.SetActive(false);
+	  RedEyes.SetActive(false);
+	  Growl.SetActive(false);
+	  Normal.SetActive(false);
+	  NormalDark.SetActive(false);
+	  OpenMouth.SetActive(false);
+      ArtBGUnderPorch.SetActive(true);
+	  ArtBGCover.SetActive(true);
+	  ArtBGCover75.SetActive(false);
+	  ArtBGCoverBroken.SetActive(false);
+      Choice4b.SetActive(false);
+      Choice4c.SetActive(false);
+	  Choice4d.SetActive(false);
+	  Choice4e.SetActive(false);
       NextScene1Button.SetActive(false);
       NextScene2Button.SetActive(false);
 	  //NextScene3Button.SetActive(false);
+	  TextBox1.SetActive(false);
+	  TextBox2.SetActive(false);
       nextButton.SetActive(true);
+	  BackFlat.SetActive(true);
  }
 
 void Update(){         // use spacebar as Next button
@@ -52,37 +77,46 @@ public void talking(){         // main story function. Players hit next to progr
       if (primeInt == 1){
               // AudioSource.Play();
       }
+	  
       else if (primeInt == 2){
-             //ArtChar1.SetActive(true);
+              ImageName.SetActive(false);
+			  RedEyes.SetActive(true);
               dialogue.SetActive(true);
-              Char1name.text = "Jeda";
-              Char1speech.text = "Wakey wakey, human.";
+              Char1name.text = "";
+              Char1speech.text = " You step down the porch to investigate. The cicada screams like the screech of a jet engine. A pair of eyes glow under the eaves of the porch.";
               Char2name.text = "";
               Char2speech.text = "";
       }
+	  
      else if (primeInt ==3){
+			ArtBGCover.SetActive(false);
+			ArtBGCover75.SetActive(true);
+			RedEyes.SetActive(false);
+			Normal.SetActive(true);
               Char1name.text = "";
-              Char1speech.text = "";
-              Char2name.text = "You";
-              Char2speech.text = "Wuh..? What happened?";
+              Char1speech.text = "You are staring right at a person’s face! Attached to the face is the body of a cicada, crawling towards you with its 6 legs";
+              Char2name.text = "";
+              Char2speech.text = "";
               //gameHandler.AddPlayerStat(1);
       }
+	  
      else if (primeInt == 4){
-              Char1name.text = "Jeda";
-              Char1speech.text = "I know I did not hit you that hard.";
+			  ImageName.SetActive(true);
+              Char1name.text = "You";
+              Char1speech.text = "The fu--";
               Char2name.text = "";
               Char2speech.text = "";
       }
      else if (primeInt == 5){
               Char1name.text = "";
               Char1speech.text = "";
-              Char2name.text = "You";
-              Char2speech.text = "Hit me? Why?";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = " hELLo tHeRe LitTle onE…";
               //gameHandler.AddPlayerStat(1);
       }
      else if (primeInt == 6){
-              Char1name.text = "Jeda";
-              Char1speech.text = "I am searching for a fugitive. Ragu Fahn.";
+              Char1name.text = "You";
+              Char1speech.text = "EEEEEK";
               Char2name.text = "";
               Char2speech.text = "";
 			  gameHandler.AddPlayerStanity(-5);
@@ -90,106 +124,256 @@ public void talking(){         // main story function. Players hit next to progr
      else if (primeInt ==7){
               Char1name.text = "";
               Char1speech.text = "";
-              Char2name.text = "You";
-              Char2speech.text = "Why do you think I know anything?";
-			  
+              Char2name.text = "Cicada Man";
+              Char2speech.text = "shhHHHhhh...YoU’LL...wAke thE NeiGHbOrs…";
 			  if (gameHandler.WhatIsPlayerSanity() < 10){
 				  primeInt = 39;
 			  }
 			  
       }
+	  
      else if (primeInt == 8){
-              Char1name.text = "Jeda";
-              Char1speech.text = "Do not play the stupid. You will take me to him.";
+			  Char1name.text = "You";
+              Char1speech.text = "What are you?";
               Char2name.text = "";
               Char2speech.text = "";
-              // Turn off "Next" button, turn on "Choice" buttons
-              nextButton.SetActive(false);
+	  }
+			  
+	 else if (primeInt == 9){
+			  Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = " Issssn’t it ObVioUS…?";
+	  }
+			  
+     else if (primeInt == 10){
+			  Char1name.text = "You";
+              Char1speech.text = "Why are you here?";
+              Char2name.text = "";
+              Char2speech.text = "";
+	  }
+	 else if (primeInt == 11){
+			  Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = "BZZZT...Ssssooo maNY qUEstionS...You’RE a CurIOUS littLE oNE...aren’t YoU?";
+	  }
+	  else if (primeInt == 12){
+			 ImageName.SetActive(false);
+			  Char1name.text = "";
+              Char1speech.text = "The bug seems a bit annoyed. You can tell by the way it furrows it’s human-like eyebrows…";
+              Char2name.text = "";
+              Char2speech.text = "";
+	  }
+	  else if (primeInt == 13){
+			ImageName.SetActive(true);
+			  Char1name.text = "You";
+              Char1speech.text = "...";
+              Char2name.text = "";
+              Char2speech.text = "";
+	  }
+	  
+	  else if (primeInt == 14){
+			  Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = "LeT mE ASk YOu sssoMeThiNg...dO you liKe Riddles?";
+	  }
+	  
+	  else if (primeInt == 15){
+			  Char1name.text = "You ";
+              Char1speech.text = "Riddles?";
+              Char2name.text = "";
+              Char2speech.text = "";
+	  }
+	  
+	  else if (primeInt == 16){
+			  Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = "InsertRiddlePoem here";
+			  nextButton.SetActive(false);
               allowSpace = false;
-              NextScene1Button.SetActive(true);
-			  NextScene2Button.SetActive(true);
+              //NextScene1Button.SetActive(true);
+			  //NextScene2Button.SetActive(true);
 			  //NextScene3Button.SetActive(true);
-              //Choice1a.SetActive(true); // function Choice1aFunct()
-              //Choice1b.SetActive(true); // function Choice1bFunct()
+              Choice4b.SetActive(true); function Choice4bFunct();
+              Choice4c.SetActive(true); function Choice4cFunct();
+	  }
+	  // ENCOUNTER AFTER CHOICE #1
+     else if (primeInt == 100){
+              Char1name.text = "You";
+              Char1speech.text = "Wow... I'm speechless...";
+              Char2name.text = "";
+              Char2speech.text = "";
       }
 	  
-	  
-	  else if (primeInt ==40){
+	  else if (primeInt == 101){
               Char1name.text = "";
               Char1speech.text = "";
-              Char2name.text = "You";
-              Char2speech.text = "The world is suddenly less stable!";
-      }
+              Char2name.text = "Cicada Man";
+              Char2speech.text = " Thank you little one...I haven't shared my poem with someone in a long time…";
+	  }
 	  
+	  else if (primeInt == 102){
+              Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = "You Have Gerty's nose... ";
+	  }
 	  
-	  
-	  
-// ENCOUNTER AFTER CHOICE #1
-     else if (primeInt == 100){
-              Char1name.text = "Jeda";
-              Char1speech.text = "Then you are no use to me, and must be silenced.";
+	  else if (primeInt == 103){
+              Char1name.text = "You";
+              Char1speech.text = "You Knew Gran?";
               Char2name.text = "";
               Char2speech.text = "";
-      }
-     else if (primeInt == 101){
-              Char1name.text = "Jeda";
-              Char1speech.text = "Come back here! Do not think you can hide from me!";
+	  }
+	  
+	  else if (primeInt == 104){
+              Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = "Yes... She was a sweetie. She listened to me sing for hours under this porch, sipping her tea and solving her crosswords.";
+	  }
+	  
+	  else if (primeInt == 105){
+              Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = "Poor little lady had a heart attack right here one evening, she never got to finish her last puzzle…";
+	  }
+	  
+	  else if (primeInt == 106){
+              Char1name.text = "You";
+              Char1speech.text = "Well...it was nice to meet you, Mr. Cicada...I’ll be going now…";
               Char2name.text = "";
               Char2speech.text = "";
-              nextButton.SetActive(false);
+	  }
+	  
+	  else if (primeInt == 107){
+              Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = " The name’s Charles, by the way.";
+	  }
+	  
+	  else if (primeInt == 108){
+              Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Charles";
+              Char2speech.text = "Stop by any time and I’ll sing for you again.";
+			  nextButton.SetActive(false);
               allowSpace = false;
               NextScene1Button.SetActive(true);
+	  }
+	 
+			//CHOICE 4C DIRECTION
+	  
+	 else if (primeInt == 200){
+			Normal.SetActive(false);
+			Growl.SetActive(true);
+              Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = "BZZZZT hoWW daRREE yoUU??";
       }
-
-     else if (primeInt == 200){
-              Char1name.text = "Jeda";
-              Char1speech.text = "Do not think you can fool me, human. Where will we find him?";
+	  
+	 else if (primeInt == 201){
+			ArtBGCover75.SetActive(false);
+			ArtBGCoverBroken.SetActive(true);
+              Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "Cicada Man";
+              Char2speech.text = "REEEEEEEEEE";
+      }
+	  
+	 else if (primeInt == 202){
+              Char1name.text = "You";
+              Char1speech.text = "Holy Sh--";
               Char2name.text = "";
               Char2speech.text = "";
       }
-     else if (primeInt == 201){
+	  
+	 else if (primeInt == 203){
+			Growl.SetActive(false);
+			OpenMouth.SetActive(true);
               Char1name.text = "";
               Char1speech.text = "";
-              Char2name.text = "You";
-              Char2speech.text = "Ragu hangs out in a rough part of town. I'll take you now.";
-              nextButton.SetActive(false);
+              Char2name.text = "Cicada Man";
+              Char2speech.text = "It’ssSSS DInNeR tiME!!";
+			  nextButton.SetActive(false);
               allowSpace = false;
-              //NextScene2Button.SetActive(true);
-      }
-   }
-
-// FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch scenes)
-      // public void Choice1aFunct(){
-      //         Char1name.text = "";
-      //         Char1speech.text = "";
-      //         Char2name.text = "You";
-      //         Char2speech.text = "I don't know what you're talking about!";
-      //         primeInt = 99;
-      //         Choice1a.SetActive(false);
-      //         Choice1b.SetActive(false);
-      //         nextButton.SetActive(true);
-      //         allowSpace = true;
-      // }
-      // public void Choice1bFunct(){
-      //         Char1name.text = "";
-      //         Char1speech.text = "";
-      //         Char2name.text = "You";
-      //         Char2speech.text = "Sure, anything you want... just lay off the club.";
-      //         primeInt = 199;
-      //         Choice1a.SetActive(false);
-      //         Choice1b.SetActive(false);
-      //         nextButton.SetActive(true);
-      //         allowSpace = true;
-      // }
-
-      public void SceneChange1(){
-             SceneManager.LoadScene("Scene_4b");
-      }
-      public void SceneChange2(){
-             SceneManager.LoadScene("Scene_4c");
+              Choice4d.SetActive(true);	function Choice4dFunct();
+			  Choice4e.SetActive(true); function Choice4eFunct();
       }
 	  
-	  //public void SceneChange3(){
-      //       SceneManager.LoadScene("Scene_2b");
-      //}
+	else if (primeInt == 210){
+			ArtAssets.SetActive(false);
+			dialogue.SetActive(false);
+			TextBox1.SetActive(true);
+			NextScene2Button.SetActive(true);
+	  }
+              
+	else if (primeInt == 220){
+			ArtAssets.SetActive(false);
+			dialogue.SetActive(false);
+			TextBox2.SetActive(true);
+			NextScene2Button.SetActive(true);		
+	  }
+}
+	  public void Choice4bFunct(){
+               Char1name.text = "You";
+               Char1speech.text = "good answer*";
+               Char2name.text = "";
+               Char2speech.text = "";
+               primeInt = 99;
+               Choice4b.SetActive(false);
+               Choice4c.SetActive(false);
+               nextButton.SetActive(true);
+               allowSpace = true;
+	  }
+	  
+	  public void Choice4cFunct(){
+               Char1name.text = "You";
+               Char1speech.text = "What kind of garbage did I just listen to? Were you even trying to rhyme?? Stick to aerating the soil, you freak!";
+               Char2name.text = "";
+               Char2speech.text = "";
+			   primeInt = 199;
+               Choice4b.SetActive(false);
+               Choice4c.SetActive(false);
+               nextButton.SetActive(true);
+               allowSpace = (true);
+	  }
+	  
+	  public void Choice4dFunct(){
+			  Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "";
+              Char2speech.text = "";
+			   primeInt = 209;
+               Choice4d.SetActive(false);
+               Choice4e.SetActive(false);
+               nextButton.SetActive(true);
+               allowSpace = true;
+	   }
+	   
+	  public void Choice4eFunct(){
+			  Char1name.text = "";
+              Char1speech.text = "";
+              Char2name.text = "";
+              Char2speech.text = "";
+			  primeInt = 219;
+              Choice4d.SetActive(false);
+              Choice4e.SetActive(false);
+              nextButton.SetActive(true);
+              allowSpace = (true);
+	   }
+	   
+       public void SceneChange1(){
+             SceneManager.LoadScene("Scene_5");
+      }
+	  
+      public void SceneChange2(){
+             SceneManager.LoadScene("LoseScreen");
+      }
 }
